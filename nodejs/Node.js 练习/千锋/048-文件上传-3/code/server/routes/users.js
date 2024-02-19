533c5735e70c04231b440807909353aee726100a
zkf990305 <sailjamie66@gmail.com>
2024-02-19 18:21:34 +0800

    feat: 文件上传

--- nodejs/Node.js 练习/千锋/048-文件上传-3/code/server/routes/users.js
+++ nodejs/Node.js 练习/千锋/048-文件上传-3/code/server/routes/users.js
@@ -3,13 +3,47 @@ const UserController = require('../controller/UserController');
 
 const router = express.Router();
 // 引入 multer
-const multer  = require('multer')
+const multer = require('multer')
 const upload = multer({ dest: 'public/uploads/' })
 /* GET users listing. */
 router.get('/', function (req, res) {
   res.send('respond with a resource');
 });
 
+/**
+ * 
+ * @api {method} /api/user 添加用户
+ * @apiName addUser
+ * @apiGroup usergroup
+ * @apiVersion  1.0.0
+ * 
+ * 
+ * @apiParam  {String} username 用户名
+ * 
+ * @apiParam  {String} password 密码
+ * 
+ * @apiParam  {Number} age 年龄
+ * 
+ * @apiParam  {File} avatar 头像
+ * 
+ * @apiSuccess (200) {Number} ok 标识成功字段
+ * 
+ * @apiParamExample  {multipart/form-data} Request-Example:
+ * {
+ *     username : "sail",
+ *     password: "123456",
+ *     age: 18,
+ *     avatar: File
+ * }
+ * 
+ * 
+ * @apiSuccessExample {type} Success-Response:
+ * {
+ *     ok : 1
+ * }
+ * 
+ * @apiSampleRequest true
+ */
 router.post('/user',
   // POST添加用户
   upload.single('avatar'),
